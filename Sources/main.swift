476195f090435267316f433d754ca8da2a5224ce
Sands <lishzh@youzu.com>
2017-04-07 14:24:01 +0800

    Working with Multiple Source Files

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,8 @@
-print("A command line tool to make your project great again.")
+//print("A command line tool to make your project great again.")
+
+if CommandLine.arguments.count != 2 {
+    print("Usage: hello NAME")
+} else {
+    let name = CommandLine.arguments[1]
+    sayHello(name: name)
+}
