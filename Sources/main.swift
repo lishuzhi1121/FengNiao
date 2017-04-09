b0c61b5acc76f05c47f72551b34f22311cfc02a3
Sands <lishzh@youzu.com>
2017-04-09 18:37:44 +0800

    add main
    main changed

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,11 +1,1 @@
-//print("A command line tool to make your project great again.")
-
-let argc = CommandLine.arguments.count
-print("\(argc)")
-
-if CommandLine.arguments.count != 2 {
-    print("Usage: hello NAME")
-} else {
-    let name = CommandLine.arguments[1]
-    sayHello(name: name)
-}
+print("A command line tool to make your project great again.")
